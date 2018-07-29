01c376f03e446da81a528135db8cd6e6f1ca3755
Alexander Ponimaskin <ponimaskin@gmail.com>
2018-07-30 00:16:39 +0300

    fighting with assignment 3

--- assignment3/src/main/java/BlockChain.java
+++ assignment3/src/main/java/BlockChain.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.TreeMap;
+
 // Block Chain should maintain only limited block nodes to satisfy the functions
 // You should not have all the blocks added to the block chain in memory 
 // as it would cause a memory overflow.
@@ -9,23 +12,40 @@ public class BlockChain {
      * create an empty block chain with just a genesis block. Assume {@code genesisBlock} is a valid
      * block
      */
+    
+    // Yeah
+    TreeMap<Integer, ArrayList<Block>> chain;
+
+    TxHandler txHandler;
+    TransactionPool txPool;
+
     public BlockChain(Block genesisBlock) {
-        // IMPLEMENT THIS
+        chain = new TreeMap<Integer, ArrayList<Block>>();
+        
+        ArrayList<Block> x = new ArrayList<Block>();
+        x.add(genesisBlock);
+        chain.put(0, x);
+
+        UTXOPool pool = new UTXOPool();
+        txHandler = new TxHandler(pool);
+
+        txPool = new TransactionPool();
     }
 
     /** Get the maximum height block */
     public Block getMaxHeightBlock() {
-        // IMPLEMENT THIS
+        ArrayList<Block> x = chain.lastEntry().getValue();
+        return x.get(0);
     }
 
     /** Get the UTXOPool for mining a new block on top of max height block */
     public UTXOPool getMaxHeightUTXOPool() {
-        // IMPLEMENT THIS
+        return txHandler.getUTXOPool();
     }
 
     /** Get the transaction pool to mine a new block */
     public TransactionPool getTransactionPool() {
-        // IMPLEMENT THIS
+        return txPool;
     }
 
     /**
@@ -41,11 +61,35 @@ public class BlockChain {
      * @return true if block is successfully added
      */
     public boolean addBlock(Block block) {
-        // IMPLEMENT THIS
+        int height = chain.lastKey();
+        int minHeight = Integer.max(height - CUT_OFF_AGE, 0);
+        boolean found = false;
+
+        
+        while (height >= minHeight) {
+            if (found) break;
+            for (Block b: chain.get(height)) {
+                if (b.getHash() == block.getPrevBlockHash()) {
+                    found = true;
+                    break;
+                }
+            }
+            height--;
+        }
+
+        
+        if (!found) return false;
+        height++;
+        ArrayList<Block> x = chain.get(height);
+        if (x == null ) x = new ArrayList<Block>();
+        
+        x.add(block);
+        chain.put(height, x);
+        return true;
     }
 
     /** Add a transaction to the transaction pool */
     public void addTransaction(Transaction tx) {
-        // IMPLEMENT THIS
+        txPool.addTransaction(tx);
     }
 }
---
+++ assignment3/src/main/java/TxHandler.java
@@ -0,0 +1,102 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class TxHandler {
+    public UTXOPool pool;
+    
+    /**
+     * Creates a public ledger whose current UTXOPool (collection of unspent transaction outputs) is
+     * {@code utxoPool}. This should make a copy of utxoPool by using the UTXOPool(UTXOPool uPool)
+     * constructor.
+     */
+    
+    public TxHandler(UTXOPool utxoPool) {
+        pool = new UTXOPool(utxoPool);
+    }
+
+    public UTXOPool getUTXOPool() {
+        return new UTXOPool(pool);
+    };
+    
+    public boolean isValidTx(Transaction tx) {
+        Set<UTXO> setUTXO = new HashSet<UTXO>();
+        double inputSum = 0;
+        double outputSum = 0;
+        
+        for (int i = 0; i < tx.numInputs(); i++) {
+            Transaction.Input in = tx.getInput(i);
+            byte[] data = tx.getRawDataToSign(i);
+            
+            UTXO u = new UTXO(in.prevTxHash, in.outputIndex);
+
+            // * (3) no UTXO is claimed multiple times by {@code tx}
+            if (!setUTXO.add(u)) {
+                return false;
+            }
+
+            Transaction.Output out = pool.getTxOutput(u);
+
+            // * (1) all outputs claimed by {@code tx} are in the current UTXO pool 
+            if (out == null) {
+                return false;
+            }
+
+            // * (2) the signatures on each input of {@code tx} are valid,
+            if (!Crypto.verifySignature(out.address, data, in.signature)) {
+                return false;
+            }
+            // 
+            inputSum += out.value;
+        }
+
+        for (Transaction.Output out: tx.getOutputs()) {
+            // * (4) all of {@code tx}s output values are non-negative, and
+
+            if (out.value < 0) {
+                return false;
+            }
+            outputSum += out.value;
+        }
+        // * (5) the sum of {@code tx}s input values is greater than or equal to the sum of its output
+        if (outputSum > inputSum) {
+            return false;
+        }
+
+        return true;
+    }
+
+
+    /**
+     * Handles each epoch by receiving an unordered array of proposed transactions, checking each
+     * transaction for correctness, returning a mutually valid array of accepted transactions, and
+     * updating the current UTXO pool as appropriate.
+     */
+    public Transaction[] handleTxs(Transaction[] possibleTxs) {
+        Set<Transaction> txs = new HashSet<Transaction>();
+        Set<Transaction.Input> inputs = new HashSet<Transaction.Input>();
+        
+        for (Transaction tx: possibleTxs) {
+            if (!isValidTx(tx)) {
+                continue;
+            }
+            
+            for (int i = 0; i < tx.numInputs(); i++) {
+                Transaction.Input in = tx.getInput(i);
+                UTXO u = new UTXO(in.prevTxHash, in.outputIndex);
+                pool.removeUTXO(u);
+            }
+
+            for (int i = 0; i < tx.numOutputs(); i++) {
+                Transaction.Output out = tx.getOutput(i);
+                UTXO u = new UTXO(tx.getHash(), i);
+                pool.addUTXO(u, out);
+            }
+
+            txs.add(tx);
+        }
+
+        
+        return txs.toArray(new Transaction[txs.size()]);
+    }
+
+}
